86a52db497b2cde0fa2731863635f39def8d3656
Jiaqi Wang <xwszc7@gmail.com>
2015-11-04 21:02:31 +0800

    change values

--- pre_define.h
+++ pre_define.h
@@ -2,9 +2,9 @@
 
 #ifndef GAME_OF_LIFE_PRE_DEFINE
 #define GAME_OF_LIFE_PRE_DEFINE
-#define CELL_X 100
-#define CELL_Y 100
-#define CELL_SIZE 10
+#define CELL_X 1024
+#define CELL_Y 1024
+#define CELL_SIZE 1
 #define CELL_COLOR (((200)<<16) | ((200)<<8) | (200))
 #define ITERATE_TIME 1000
 //#define BLOCK_DIM 1024
