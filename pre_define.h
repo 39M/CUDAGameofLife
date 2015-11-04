f5ddde1370ee27a1ff58dfddd9b0f5094dcd7c53
Jiaqi Wang <xwszc7@gmail.com>
2015-11-04 22:46:29 +0800

    change parameter

--- pre_define.h
+++ pre_define.h
@@ -2,8 +2,8 @@
 
 #ifndef GAME_OF_LIFE_PRE_DEFINE
 #define GAME_OF_LIFE_PRE_DEFINE
-#define CELL_X 1024
-#define CELL_Y 1024
+#define CELL_X 800
+#define CELL_Y 800
 #define CELL_SIZE 1
 #define CELL_COLOR (((200)<<16) | ((200)<<8) | (200))
 #define ITERATE_TIME 1000
