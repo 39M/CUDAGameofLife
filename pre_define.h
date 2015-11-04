a56190549e443229f66c3ed9b2ebaada237727f3
Jiaqi Wang <xwszc7@gmail.com>
2015-11-04 20:38:59 +0800

    pre-defines

---
+++ pre_define.h
@@ -0,0 +1,11 @@
+/* Pre-defines of Game of Life */
+
+#ifndef GAME_OF_LIFE_PRE_DEFINE
+#define GAME_OF_LIFE_PRE_DEFINE
+#define CELL_X 100
+#define CELL_Y 100
+#define CELL_SIZE 10
+#define CELL_COLOR (((200)<<16) | ((200)<<8) | (200))
+#define ITERATE_TIME 1000
+//#define BLOCK_DIM 1024
+#endif
